1c51decd104b265c3bb47e2b8f89b93d496787a6
Mustafa Panjiwala <56435815+mustafapanjiwala@users.noreply.github.com>
2021-08-04 14:27:27 +0530

    login ready

--- login.php
+++ login.php
@@ -1,4 +1,6 @@
-<?php 
+<?php
+session_start(); 
+include "db_conn.php";
 if (isset($_POST['its']) && isset($_POST['password'])){
     function validate($data){
         $data = trim($data);
@@ -18,7 +20,26 @@ if (isset($_POST['its']) && isset($_POST['password'])){
         exit();
     }
     else{
-        echo 'Valid input';
+        $sql = "SELECT * FROM users WHERE its ='$its' AND password = '$pass'";
+
+        $result = mysqli_query($conn ,$sql);
+
+        if (mysqli_num_rows($result) === 1) {
+            $row = mysqli_fetch_assoc($result);
+            if ($row['its']=== $its && $row['password']=== $pass) {
+                $_SESSION['its'] = $row['its'];
+                $_SESSION['name'] = $row['name'];
+                $_SESSION['id'] = $row['id'];
+                header("Location:home.php");
+                exit();
+            }else{
+                header("Location:index.php?error=Incorrect ITS ID or Password");
+                exit();
+            }
+        }else{
+            header("Location:index.php?error=Incorrect ITS ID or Password");
+            exit();
+        }
     }
 }else{
     header("Location:index.php");
